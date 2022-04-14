f2bdb62c8f523a241e8a2eca6a728c69fb04f41a
Josue Daniel Vallejo Coss <jdanielvcoss@gmail.com>
2022-04-13 19:33:49 -0500

    :)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,6 +22,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -28,7 +30,9 @@
                 <a class="navbar-brand" href="{{ url('/home') }}">
                     Control de Asistencia
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -41,7 +45,11 @@
                                     <a class="nav-link text-black" href="/calendario">Calendario</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link text-black" href="#" data-bs-toggle="modal" data-bs-target="#importFile">Importar CSV</a>
+                                    <a class="nav-link text-black" href="/salones">Salones</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-black" href="#" data-bs-toggle="modal"
+                                        data-bs-target="#importFile">Importar CSV</a>
                                 </li>
                             </ul>
                         </ul>
@@ -49,7 +57,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
-                        
+
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -58,34 +66,36 @@
                             @endif
 
                             @if (Route::has('register'))
-                                
                             @endif
-                            @else
-                                <div class="">
-                                    <form class="d-flex" action="" method="get">
-                                        @csrf
-                                        <input class="form-control me-2 " type="search" placeholder="Buscar Empleado" aria-label="Buscar" name="search">
-                                        <button class="btn btn-primary me-3" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                                    </form>
-                                </div>
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }}
+                        @else
+                            <div class="">
+                                <form class="d-flex" action="" method="get">
+                                    @csrf
+                                    <input class="form-control me-2 " type="search" placeholder="Buscar Empleado"
+                                        aria-label="Buscar" name="search">
+                                    <button class="btn btn-primary me-3" type="submit"><i
+                                            class="fa-solid fa-magnifying-glass"></i></button>
+                                </form>
+                            </div>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('register') }}">Registrar a alguien</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                        Cerrar Sesion
                                     </a>
 
-                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('register') }}">Registrar a alguien</a>
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                            Cerrar Sesion
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
                         @endguest
                     </ul>
                 </div>
@@ -96,26 +106,27 @@
         </main>
     </div>
 
-  <!-- Modal -->
-  <div class="modal fade" id="importFile" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Importar CSV</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <!-- Modal -->
+    <div class="modal fade" id="importFile" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Importar CSV</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="/importarCSV" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-body">
+                        <input class="form-control" type="file" name="csv" id="csv" accept=".csv" required>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <button type="button submit" class="btn btn-primary">Guardar Archivo</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <form action="/importarCSV" method="post" enctype="multipart/form-data">
-            @csrf
-            <div class="modal-body">
-                <input class="form-control" type="file" name="csv" id="csv" accept=".csv" required>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="button submit" class="btn btn-primary">Guardar Archivo</button>
-              </div>
-        </form>
-      </div>
     </div>
-  </div>
 </body>
+
 </html>
